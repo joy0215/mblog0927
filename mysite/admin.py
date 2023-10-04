2a8ea21f002252c8d77340c6c79014dff2f8f565
joy0215 <11056017@ntub.edu.tw>
2023-10-04 15:51:44 +0800

    add post(minji)project 我只能永遠讀著對白

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from mysite.models import Minji
 # Register your models here.
+class MinjiAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','pub_date')
+
+admin.site.register(Minji,MinjiAdmin)
+

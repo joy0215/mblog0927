691e7428e69539647aa16b52a92e1f7803a4ebd0
joy0215 <11056017@ntub.edu.tw>
2023-11-01 16:12:04 +0800

    今天其實沒教甚麼

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from mysite.models import Minji
+from mysite.models import Minji , Product
 # Register your models here.
 class MinjiAdmin(admin.ModelAdmin):
     list_display = ('title','slug','pub_date')
 
 admin.site.register(Minji,MinjiAdmin)
-
+admin.site.register(Product)

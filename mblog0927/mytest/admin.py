f616968e9019f5f1d83c333ab84bb2456427ecf3
joy0215 <11056017@ntub.edu.tw>
2023-12-27 13:50:19 +0800

    use teacher's

--- mblog0927/mytest/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from mytest.models import Post, Mood
-# Register your models here.
-
-class PostAdmin(admin.ModelAdmin):
-    list_display=('nickname', 'message', 'del_pass', 'enabled', 'pub_time')
-   
-admin.site.register(Post, PostAdmin)
-admin.site.register(Mood)
